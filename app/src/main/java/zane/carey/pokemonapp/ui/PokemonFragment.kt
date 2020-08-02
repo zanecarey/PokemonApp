837ead3dd7aeeda6ddc2da7b89bd1aff91eea25c
Zane Carey <zanecarey1@gmail.com>
2020-08-01 22:51:24 -0700

    Added Koin dependency

---
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonFragment.kt
@@ -0,0 +1,26 @@
+package zane.carey.pokemonapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class PokemonFragment: Fragment(){
+
+    private val pokeViewModel: PokemonViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
