16e4ffd36534f26b2b5228a376580a9898f43f0c
Zane Carey <zanecarey1@gmail.com>
2020-08-01 22:22:55 -0700

    Added listitem for pokemon recyclerview adapter

--- app/src/main/java/zane/carey/pokemonapp/ui/PokemonAdapter.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonAdapter.kt
@@ -5,18 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.poke_list_item.view.*
+import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.model.Pokemon
 
-class PokemonAdapter(val context: Context) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>(){
+class PokemonAdapter(val pokeList: List<Pokemon>, val context: Context) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>(){
 
 
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate())
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.poke_list_item, parent, false))
     }
 
     override fun getItemCount(): Int {
-        return
+        return pokeList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -25,5 +28,8 @@ class PokemonAdapter(val context: Context) : RecyclerView.Adapter<PokemonAdapter
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+        val name = view.pokemonName
+        val type1 = view.type1
+        val type2 = view.type2
     }
 }
