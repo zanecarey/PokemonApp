664ec50b0f9b063d11f1e24aa1089e0c09550802
Zane Carey <zanecarey1@gmail.com>
2020-08-22 20:23:15 -0700

    Added navigation setup for main screen, different fragment packages

---
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonSearch/PokemonSearchFragment.kt
@@ -0,0 +1,18 @@
+package zane.carey.pokemonapp.ui.PokemonSearch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+
+class PokemonSearchFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
