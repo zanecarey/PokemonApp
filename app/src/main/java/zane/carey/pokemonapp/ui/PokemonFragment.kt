3587f9aca3f321c6afe14f3d4417b94f04fb4fde
Zane Carey <zanecarey1@gmail.com>
2020-09-24 00:33:38 -0700

    Added navigations

--- app/src/main/java/zane/carey/pokemonapp/ui/PokemonFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonFragment.kt
@@ -5,22 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.fragment_pokemon.*
 import org.koin.android.viewmodel.ext.android.viewModel
+import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.model.Pokemon
+import zane.carey.pokemonapp.ui.ViewPager.ViewPagerViewModel
 
 class PokemonFragment: Fragment(){
 
-    private val pokeViewModel: PokemonViewModel by viewModel()
+    private lateinit var pokeViewModel: PokemonViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pokeViewModel = ViewModelProvider(this).get(PokemonViewModel::class.java)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_poke_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val pokemonRecyclerView = recyclerView
+        val layoutManager = GridLayoutManager(context, 2)
+        pokemonRecyclerView.layoutManager = layoutManager
+
+        pokeViewModel.getPokemonList().observe(viewLifecycleOwner, Observer {
+            val pokemons: List<Pokemon> = it
+            recyclerView.adapter = PokemonAdapter(pokemons, view.context)
+        })
     }
 }
