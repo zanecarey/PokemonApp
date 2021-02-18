24a20def5f9602a72fb44fc9d9a7da9cdbc1c2c6
Zane Carey <zanecarey1@gmail.com>
2021-02-18 00:13:03 -0800

    Added regioninterface data classes

--- app/src/main/java/zane/carey/pokemonapp/repository/RegionInterface.kt
+++ app/src/main/java/zane/carey/pokemonapp/repository/RegionInterface.kt
@@ -1,4 +1,66 @@
 package zane.carey.pokemonapp.repository
 
+import zane.carey.pokemonapp.model.GenerationGameIndex
+import zane.carey.pokemonapp.model.Name
+import zane.carey.pokemonapp.model.NamedApiResource
+import zane.carey.pokemonapp.model.VersionEncounterDetail
+
 class RegionInterface {
+    data class Region(
+        val id: Int,
+        val name: String,
+        val locations: List<NamedApiResource>,
+        val main_generation: NamedApiResource,
+        val names: List<Name>,
+        val pokedexes: List<NamedApiResource>,
+        val versionGroups: List<NamedApiResource>
+    )
+
+    data class Location(
+        val id: Int,
+        val name: String,
+        val region: NamedApiResource?,
+        val names: List<Name>,
+        val game_indices: List<GenerationGameIndex>,
+        val areas: List<NamedApiResource>
+    )
+
+    data class LocationArea(
+        val id: Int,
+        val name: String,
+        val gameIndex: Int,
+        val encounterMethodRates: List<EncounterMethodRate>,
+        val location: NamedApiResource,
+        val names: List<Name>,
+        val pokemonEncounters: List<PokemonEncounter>
+    )
+
+    data class EncounterMethodRate(
+        val encounterMethod: NamedApiResource,
+        val versionDetails: List<EncounterMethodRateVersionDetail>
+    )
+
+    data class EncounterMethodRateVersionDetail(
+        val rate: Int,
+        val version: NamedApiResource
+    )
+
+    data class PokemonEncounter(
+        val pokemon: NamedApiResource,
+        val versionDetails: List<VersionEncounterDetail>
+    )
+
+    data class PalParkArea(
+        val id: Int,
+        val name: String,
+        val names: List<Name>,
+        val pokemonEncounters: List<PalParkEncounterSpecies>
+    )
+
+    data class PalParkEncounterSpecies(
+        val baseScore: Int,
+        val rate: Int,
+        val pokemonSpecies: NamedApiResource
+    )
+
 }
--- app/src/main/java/zane/carey/pokemonapp/ui/moves/MoveFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/moves/MoveFragment.kt
@@ -1,4 +1,52 @@
 package zane.carey.pokemonapp.ui.moves
 
-class MoveFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.fragment_item.*
+import kotlinx.android.synthetic.main.fragment_item.itemRecyclerView
+import kotlinx.android.synthetic.main.fragment_moves.*
+import zane.carey.pokemonapp.App
+import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.database.PokeDAO
+import zane.carey.pokemonapp.model.PokeItem
+import zane.carey.pokemonapp.model.PokemonMove
+import zane.carey.pokemonapp.ui.items.ItemAdapter
+import zane.carey.pokemonapp.ui.items.ItemViewModel
+
+class MoveFragment: Fragment() {
+
+    private lateinit var moveViewModel: MoveViewModel
+    private val pokeDao: PokeDAO = App.database.pokeDao()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        moveViewModel = ViewModelProvider(this).get(MoveViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_moves, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val moveRecyclerView = moveRecyclerView
+        val layoutManager = GridLayoutManager(context, 2)
+        moveRecyclerView.layoutManager = layoutManager
+
+        moveViewModel.getMoveList().observe(viewLifecycleOwner, Observer {
+            val moves: List<PokemonMove> = it
+            moveRecyclerView.adapter = MoveAdapter(moves, view.context)
+        })
+    }
 }
