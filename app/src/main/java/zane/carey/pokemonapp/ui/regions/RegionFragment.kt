fe2143726a18cf8898b5bc4a01d98c9672c9e564
Zane Carey <zanecarey1@gmail.com>
2021-02-18 00:05:52 -0800

    Implemented RegionAdapter.kt onViewCreated

--- app/src/main/java/zane/carey/pokemonapp/ui/regions/RegionFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/regions/RegionFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_region.*
 import zane.carey.pokemonapp.App
 import zane.carey.pokemonapp.R
 import zane.carey.pokemonapp.database.PokeDAO
 import zane.carey.pokemonapp.model.PokeItem
+import zane.carey.pokemonapp.model.Region
 import zane.carey.pokemonapp.ui.items.ItemAdapter
 import zane.carey.pokemonapp.ui.items.ItemViewModel
 
@@ -29,6 +33,19 @@ class RegionFragment: Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_item, container, false)
+        return inflater.inflate(R.layout.fragment_region, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val regionRecyclerView = regionRecyclerView
+        val layoutManager = GridLayoutManager(context, 2)
+        regionRecyclerView.layoutManager = layoutManager
+
+        regionViewModel.getRegionList().observe(viewLifecycleOwner, Observer {
+            val regions: List<Region> = it
+            regionRecyclerView.adapter = RegionAdapter(regions, view.context)
+        })
     }
 }
