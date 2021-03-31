466e210d102ba9a30f8a0ec841110ef41400f325
Zane Carey <zanecarey1@gmail.com>
2021-03-30 23:54:59 -0700

    Implemented ItemDisplay

--- app/src/main/java/zane/carey/pokemonapp/ui/items/ItemDisplay.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/items/ItemDisplay.kt
@@ -1,6 +1,39 @@
 package zane.carey.pokemonapp.ui.items
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_item_display.*
+import zane.carey.pokemonapp.App
+import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.database.PokeDAO
 
 class ItemDisplay: Fragment() {
+
+    private val pokeDao: PokeDAO = App.database.pokeDao()
+
+    val itemID = arguments?.getString("id")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_item_display, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        pokeDao.getItemById(itemID).observe(viewLifecycleOwner, Observer { itemValue ->
+            itemName.text = itemValue.name
+            itemFlavorText.text = itemValue.flavorTextEntry
+            itemCost.text = itemValue.cost.toString()
+
+        })
+    }
+
 }
