9bbedbd4ff8d3506db7fff8e3e7401218c03040f
zanec <zanecarey1@gmail.com>
2021-06-10 21:09:16 -0700

    Added search functionality

--- app/src/main/java/zane/carey/pokemonapp/ui/PokemonSearch/SearchFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonSearch/SearchFragment.kt
@@ -1,5 +1,8 @@
 package zane.carey.pokemonapp.ui.PokemonSearch
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +10,24 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_poke_search.*
 import kotlinx.android.synthetic.main.fragment_poke_search.view.*
+import kotlinx.android.synthetic.main.poke_list_item.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.model.EvolutionChain
+import zane.carey.pokemonapp.model.Pokemon
 import zane.carey.pokemonapp.repository.PokeAPI
+import zane.carey.pokemonapp.repository.PokemonResults
+import zane.carey.pokemonapp.repository.PokemonSpecies
+import zane.carey.pokemonapp.ui.PokemonAdapter
+import zane.carey.pokemonapp.ui.PokemonFragment
+import zane.carey.pokemonapp.ui.PokemonViewModel
 
 class SearchFragment: Fragment() {
     override fun onCreateView(
@@ -37,6 +50,17 @@ class SearchFragment: Fragment() {
                 withContext(Dispatchers.Main) {
                     searchEntry.setText(results.id)
 
+                    val intent = Intent(this, PokemonFragment::class.java)
+                    intent.putExtra("pokemonID", results.id)
+
+
+                    val builder = AlertDialog.Builder(context)
+                    builder.setTitle("Results")
+                    builder.setPositiveButton("Ok"){ dialogInterface: DialogInterface, i: Int ->
+
+                    }
+
+
                 }
             } //update ui info
 
@@ -45,4 +69,6 @@ class SearchFragment: Fragment() {
     }
 
     suspend fun getPoke(id : String) = PokeAPI.pokemonService.getPokemon(id)
+
+
 }
