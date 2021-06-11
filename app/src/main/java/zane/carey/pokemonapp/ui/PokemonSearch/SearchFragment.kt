a174297416d5621f89836bd3db084c19dbfbc4ab
zanec <zanecarey1@gmail.com>
2021-06-10 23:58:51 -0700

    Fixed search functionality

--- app/src/main/java/zane/carey/pokemonapp/ui/PokemonSearch/SearchFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonSearch/SearchFragment.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_poke_search.*
@@ -29,7 +31,7 @@ import zane.carey.pokemonapp.ui.PokemonAdapter
 import zane.carey.pokemonapp.ui.PokemonFragment
 import zane.carey.pokemonapp.ui.PokemonViewModel
 
-class SearchFragment: Fragment() {
+class SearchFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -45,30 +47,27 @@ class SearchFragment: Fragment() {
             val entry = view.searchEntry.text.toString()
             //view.searchTest.text = entry
             //make api call with entered name
-            val job = CoroutineScope(Dispatchers.Main).launch{
+            val job = CoroutineScope(Dispatchers.Main).launch {
                 val results = getPoke(entry)
                 withContext(Dispatchers.Main) {
                     searchEntry.setText(results.id)
 
-                    val intent = Intent(this, PokemonFragment::class.java)
-                    intent.putExtra("pokemonID", results.id)
 
-
-                    val builder = AlertDialog.Builder(context)
-                    builder.setTitle("Results")
-                    builder.setPositiveButton("Ok"){ dialogInterface: DialogInterface, i: Int ->
-
-                    }
-
-
+                    var idBundle = bundleOf("id" to results.id)
+                    it.findNavController()
+                        .navigate(
+                            R.id.action_navigation_main_to_navigation_poke_viewpager,
+                            idBundle
+                        )
                 }
-            } //update ui info
 
-
+            }
         }
+
+
     }
 
-    suspend fun getPoke(id : String) = PokeAPI.pokemonService.getPokemon(id)
+    suspend fun getPoke(id: String) = PokeAPI.pokemonService.getPokemon(id)
 
 
 }
