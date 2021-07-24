a7c76305911e39b1c7dd7483b6bda0bcc403c6c1
zanec <zanecarey1@gmail.com>
2021-07-23 22:06:22 -0700

    Fixed type 2 display room

--- app/src/main/java/zane/carey/pokemonapp/ui/PokemonAdapter.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonAdapter.kt
@@ -26,7 +26,7 @@ class PokemonAdapter(private val pokeList: List<Pokemon>, private val context: C
                     .navigate(R.id.action_navigation_pokemon_to_navigation_viewpager, idBundle)
             }
             itemView.type1.text = item.type?.get(0).toString()
-            itemView.type2.text = item.type?.get(1).toString()
+            itemView.type2.text = item.type?.getOrNull(1).toString()
 
             Glide.with(itemView.context)
                 .load(item.sprites?.get(0))
--- app/src/main/java/zane/carey/pokemonapp/util/StringConverter.kt
+++ app/src/main/java/zane/carey/pokemonapp/util/StringConverter.kt
@@ -3,11 +3,13 @@ package zane.carey.pokemonapp.util
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import zane.carey.pokemonapp.repository.PokemonType
 import java.lang.reflect.Type
 
 class StringConverter {
     val gson = Gson()
     val type: Type = object : TypeToken<List<String?>?>() {}.type
+    val type2: Type = object : TypeToken<List<PokemonType?>?>() {}.type
 
     @TypeConverter
     fun fromString(json: String?): List<String> {
@@ -17,5 +19,15 @@ class StringConverter {
     @TypeConverter
     fun fromList(list: List<String?>?): String {
         return gson.toJson(list, type)
+    }
+
+    @TypeConverter
+    fun fromTypeString(json: String?): List<PokemonType> {
+        return gson.fromJson(json, type2)
+    }
+
+    @TypeConverter
+    fun fromTypeList(list: List<PokemonType?>?): String {
+        return gson.toJson(list, type2)
     }
 }
