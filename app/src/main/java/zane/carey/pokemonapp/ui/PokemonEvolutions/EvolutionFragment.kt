1ef51d0471cb4eede490b5350528f15635f8165c
zanec <zanecarey1@gmail.com>
2021-08-02 19:26:23 -0700

    Added onclicklistener for evolution picture

--- app/src/main/java/zane/carey/pokemonapp/ui/PokemonEvolutions/EvolutionFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/PokemonEvolutions/EvolutionFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_poke_bio.*
 import kotlinx.android.synthetic.main.fragment_poke_evolutions.*
+import kotlinx.android.synthetic.main.fragment_viewpager.*
 import kotlinx.android.synthetic.main.poke_list_item.view.*
 import zane.carey.pokemonapp.R
 import zane.carey.pokemonapp.ui.PokemonStats.StatsFragment
+import zane.carey.pokemonapp.ui.ViewPager.ViewPagerAdapter
+import zane.carey.pokemonapp.ui.ViewPager.ViewPagerFragment
 import zane.carey.pokemonapp.ui.ViewPager.ViewPagerViewModel
 
 class EvolutionFragment: Fragment() {
@@ -48,21 +54,26 @@ class EvolutionFragment: Fragment() {
         vpViewModel.getById(pokeID).observe(viewLifecycleOwner, Observer { pokemonValue ->
             pokemonValue?.let { pokemon ->
 
-                firstEvolutionTextView.text = pokemon.evolutionChain?.get(0).toString().capitalize()
-                vpViewModel.getByName(pokemon.evolutionChain?.get(0).toString()).observe(viewLifecycleOwner, Observer { evoValue ->
+                //get images for the first evolution pokemon
+                val evoName = pokemon.evolutionChain?.get(0).toString().capitalize()
+                firstEvolutionTextView.text = evoName
+                vpViewModel.getByName(evoName.toLowerCase()).observe(viewLifecycleOwner, Observer { evoValue ->
                     evoValue?.let { poke ->
                         Glide.with(this)
                             .load(poke.sprites?.get(0))
                             .placeholder(android.R.color.transparent)
                             .into(firstEvoImage)
+
+                        firstEvoImage.setOnClickListener{
+
+                            var idBundle = bundleOf("name" to evoName)
+                            it.findNavController()
+                                .navigate(R.id.action_navigation_evolutionFragment_to_viewpager, idBundle)
+                        }
                     }
                 })
-
-                //typeTextView.text = pokemon.type.toString()
-//                weightTextView.text = pokemon.weight.toString()
-//                heightTextView.text = pokemon.height.toString()
-//                baseXPTextView.text = pokemon.baseXP.toString()
             }
         })
+
     }
 }
--- app/src/main/java/zane/carey/pokemonapp/ui/ViewPager/ViewPagerFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/ViewPager/ViewPagerFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_poke_evolutions.*
 import kotlinx.android.synthetic.main.fragment_viewpager.*
 import zane.carey.pokemonapp.R
 
@@ -20,6 +24,8 @@ class ViewPagerFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vpViewModel = ViewModelProvider(this).get(ViewPagerViewModel::class.java)
+
+
     }
 
     override fun onCreateView(
@@ -33,7 +39,19 @@ class ViewPagerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val pokeID = arguments?.getString("id")
+        val evoName = arguments?.getString("name")
+        var pokeID = "4"
+        if(!evoName.isNullOrEmpty()){
+            vpViewModel.getByName(evoName).observe(viewLifecycleOwner, Observer { evoValue ->
+                evoValue?.let { poke ->
+                    pokeID = poke.id
+                }
+            })
+        } else {
+            pokeID = arguments?.getString("id").toString()
+        }
+        //val pokeID = arguments?.getString("id")
+
         vpViewModel.getById(pokeID).observe(viewLifecycleOwner, Observer { pokemonValue ->
             pokemonValue?.let { pokemon ->
 
@@ -59,4 +77,6 @@ class ViewPagerFragment : Fragment() {
             }
         })
     }
+
+
 }
