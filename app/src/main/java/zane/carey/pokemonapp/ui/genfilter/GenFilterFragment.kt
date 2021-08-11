ef35f91274d508d067f2a658f47ca0bdc7861d7e
zanec <zanecarey1@gmail.com>
2021-08-10 23:38:09 -0700

    Impemented GenFilterFragment.kt

--- app/src/main/java/zane/carey/pokemonapp/ui/genfilter/GenFilterFragment.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/genfilter/GenFilterFragment.kt
@@ -1,6 +1,41 @@
 package zane.carey.pokemonapp.ui.genfilter
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_gen_filter.*
+import org.koin.android.viewmodel.compat.ViewModelCompat.viewModel
+import org.koin.android.viewmodel.ext.android.viewModel
+import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.model.Generation
 
-class GenFilterFragment: Fragment() {
+class GenFilterFragment: BottomSheetDialogFragment() {
+
+    private val generationViewModel: GenFilterViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_generation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = genFilterRecyclerView
+        val layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = layoutManager
+
+        generationViewModel.getListGeneration().observe(viewLifecycleOwner, Observer {
+            val pokemons: List<Generation> = it
+            recyclerView.adapter = GenFilterAdapter(pokemons, view.context)
+        })
+    }
 }
--- app/src/main/java/zane/carey/pokemonapp/ui/genfilter/GenFilterViewModel.kt
+++ app/src/main/java/zane/carey/pokemonapp/ui/genfilter/GenFilterViewModel.kt
@@ -1,7 +1,59 @@
 package zane.carey.pokemonapp.ui.genfilter
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import zane.carey.pokemonapp.R
+import zane.carey.pokemonapp.model.Generation
 
-class GenFilterViewModel : ViewModel() {
+class GenFilterViewModel(private val context: Context) : ViewModel() {
 
+    private val listGeneration = MutableLiveData<List<Generation>>()
+
+    fun getListGeneration(): LiveData<List<Generation>> {
+        listGeneration.value = listOf(
+            Generation(
+                1,
+                context.resources.getString(R.string.gen1),
+                R.drawable.gen1
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen2),
+                R.drawable.gen2
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen3),
+                R.drawable.gen3
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen4),
+                R.drawable.gen4
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen5),
+                R.drawable.gen5
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen6),
+                R.drawable.gen6
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen7),
+                R.drawable.gen7
+            ),
+            Generation(
+                1,
+                context.resources.getString(R.string.gen8),
+                R.drawable.gen8
+            )
+        )
+        return listGeneration
+    }
 }
